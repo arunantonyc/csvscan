6ed017752b09f860ebd0e0f44447a78729ce56a3
aa <arunantonyc@gmail.com>
2018-05-22 12:11:41 +0530

    Wild Card Search Filters
    Search for starts-with,  ends-with & contains using '*' on the input filters.

---
+++ StringComparer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csvscan
+{
+    public class StringComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            // checks for absolute search string
+            if (!x.StartsWith("*") && !x.EndsWith("*"))
+            {
+                return y == x;
+            }
+
+            var xFixedStr = x.Replace("*", "");
+            // checking for starts with
+            if (!x.StartsWith("*") && x.EndsWith("*"))
+            {
+                return y.StartsWith(xFixedStr);
+            }
+            // checking for ends with
+            if (x.StartsWith("*") && !x.EndsWith("*"))
+            {
+                return y.EndsWith(xFixedStr);
+            }
+            // checking for contains
+            if (x.StartsWith("*") && x.EndsWith("*"))
+            {
+                return y.Contains(xFixedStr);
+            }
+            return false;
+        }
+
+        public int GetHashCode(string x)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(x, null)) return 0;
+
+            return x.GetHashCode();
+        }
+    }
+}
